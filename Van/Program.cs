04aac5e210da1e2d0342a80675957454ac93388c
UwUssimo Robinson <uwussimo@icloud.com>
2021-11-17 02:33:05 +0500

    final touches with Van

--- Van/Program.cs
+++ Van/Program.cs
@@ -6,31 +6,24 @@ namespace Van
     {
         private static void Main()
         {
+            Console.WriteLine("Welcome to the Car Dealership!");
+
             CarList cars = new CarList();
             cars.AddCar(0, "Passenger", 10000, "Ford Focus Zetec Edition", "Chevrolet", "Excellent", 2000);
             cars.AddCar(1, "Passenger", 7000, "Nexia 3", "Chevrolet", "Good", 2019);
             cars.AddCar(2, "Industrial", 7000, "Citaro", "Mercedez", "Good", 2010);
             
-            Console.WriteLine("Welcome to the Car Dealership!");
             Console.WriteLine("Here is a list of our cars:");
             cars.DisplayCars();
+            Console.Write("\n");
             
-            Console.WriteLine("\n");
-            // The moment of truth my dear slaves!
-            
-            Console.WriteLine("What would you like to search?");
+            Console.Write("What would you like to search: ");
             var searchInputModel = Console.ReadLine();
             var search = cars.GetCarsByArguments(model: searchInputModel);
             Console.WriteLine("Here is a list of our cars matching your search:");
             foreach (var car in search)
             {
                 Console.WriteLine(car);
-            }
-            
-            // YAY IT WORKS!!! UWU GANG RISE UP!!!
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("UwU OwO UwU OwO UwU OwO UwU");
             }
         }
     }
