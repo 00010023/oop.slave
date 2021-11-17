9a08465b1892584afc8e69a9a698ead3cbb40739
UwUssimo Robinson <uwussimo@icloud.com>
2021-11-17 10:49:20 +0500

    example changed

--- Billy/Program.cs
+++ Billy/Program.cs
@@ -8,7 +8,7 @@ namespace Billy
         public static void Main()
         {
             Client client = new Client();
-            client.AddIndustrial(0, "Ford Focus RS", 7000, "Excellent", 2015, "Sedan", "Ford");
+            client.AddIndustrial(0, "Ford Focus RS", 4000, "Bad", 2015, "Sedan", "Ford");
             client.AddIndustrial(1, "Ford Mustang", 5000, "Good", 2015, "Sedan", "Ford");
             client.AddPassenger(2, "Tesla Roadster", 3000, "OK", 2020, "Sedan", "Tesla");
 
